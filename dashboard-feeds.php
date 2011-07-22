e03b61a014ecdc3a9f43c7cd719d5c486dd69189
andrey <andrey@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-07-22 09:10:00 +0000

    Dashboard-Feeds v1.5.2 - added feature for update of widgets when changed values of options in the file 'dashboard-feeds.php'
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/dashboard-feeds@3822 f53fe46e-8c02-459b-9fdd-93e75771b278

--- dashboard-feeds.php
+++ dashboard-feeds.php
@@ -4,15 +4,15 @@
 Plugin Name: Dashboard Feeds
 Plugin URI: http://premium.wpmudev.org/project/dashboard-feeds
 Description: Customize the dashboard for every user in a flash with this straightforward dashboard feed replacement widget... no more WP development news or Matt's latest photo set :)
-Author: Ivan Shaovchev, Andrew Billits (Incsub)
-Author URI: http://ivan.sh
-Version: 1.5.1
+Author: Ivan Shaovchev, Andrew Billits, Andrey Shipilov (Incsub)
+Author URI: http://premium.wpmudev.org
+Version: 1.5.2
 Network: true
 WDP ID: 15
 License: GNU General Public License (Version 2 - GPLv2)
 */
 
-/* 
+/*
 Copyright 2007-2011 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
@@ -55,8 +55,35 @@ function df_set_widget_options() {
     $widget_options['dashboard_secondary']['show_date']    = false;
 
     $current_widget_options = get_option('dashboard_widget_options');
-    $widget_options = array_merge( $current_widget_options, $widget_options );
+
+    if ( $current_widget_options ) {
+        //delete cache for update the widget when we change options values
+        if ( $current_widget_options['dashboard_primary']['link']           != $widget_options['dashboard_primary']['link'] ||
+             $current_widget_options['dashboard_primary']['url']            != $widget_options['dashboard_primary']['url'] ||
+             $current_widget_options['dashboard_primary']['items']          != $widget_options['dashboard_primary']['items'] ||
+             $current_widget_options['dashboard_primary']['show_summary']   != $widget_options['dashboard_primary']['show_summary'] ||
+             $current_widget_options['dashboard_primary']['show_author']    != $widget_options['dashboard_primary']['show_author'] ||
+             $current_widget_options['dashboard_primary']['show_date']      != $widget_options['dashboard_primary']['show_date'] )
+        {
+            $cache_key = 'dash_' . md5( 'dashboard_primary' );
+            delete_transient( $cache_key );
+        }
+        //delete cache for update the widget when we change values of options
+        if ( $current_widget_options['dashboard_secondary']['link']         != $widget_options['dashboard_secondary']['link'] ||
+             $current_widget_options['dashboard_secondary']['url']          != $widget_options['dashboard_secondary']['url'] ||
+             $current_widget_options['dashboard_secondary']['items']        != $widget_options['dashboard_secondary']['items'] ||
+             $current_widget_options['dashboard_secondary']['show_summary'] != $widget_options['dashboard_secondary']['show_summary'] ||
+             $current_widget_options['dashboard_secondary']['show_author']  != $widget_options['dashboard_secondary']['show_author'] ||
+             $current_widget_options['dashboard_secondary']['show_date']    != $widget_options['dashboard_secondary']['show_date'] )
+        {
+            $cache_key = 'dash_' . md5( 'dashboard_secondary' );
+            delete_transient( $cache_key );
+        }
+
+        $widget_options = array_merge( $current_widget_options, $widget_options );
+    }
     update_option( 'dashboard_widget_options', $widget_options );
+
 }
 
 /* Call the function */
